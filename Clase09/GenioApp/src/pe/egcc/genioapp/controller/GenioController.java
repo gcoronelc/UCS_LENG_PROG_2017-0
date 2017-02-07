aa20d370851bb53dc362ec14394ad53bbaead996
Gustavo Coronel <gcoronelc@gmail.com>
2017-02-06 22:20:38 -0500

    Clase 09

---
+++ Clase09/GenioApp/src/pe/egcc/genioapp/controller/GenioController.java
@@ -0,0 +1,5 @@
+package pe.egcc.genioapp.controller;
+
+public class GenioController {
+  
+}
---
+++ Clase09/GenioApp/src/pe/egcc/genioapp/service/MyMath.java
@@ -0,0 +1,28 @@
+package pe.egcc.genioapp.service;
+
+public final class MyMath {
+
+  private MyMath() {
+  }
+
+  public static long factorial(int n) {
+    return 0;
+  }
+
+  public static int mcd(int n1, int n2) {
+    return 0;
+  }
+
+  public static int mcm(int n1, int n2) {
+    return 0;
+  }
+
+  public static int[] fibonacci(int n) {
+    return null;
+  }
+
+  public static boolean esPrimo(int n) {
+    return false;
+  }
+
+}
